578001ecaf0f30cd48bd584f28db6e03862a6362
Prince Patel <princeamareliya8271@gmail.com>
2026-08-11 16:53:00 +0530

    Refactor components to remove unused imports and improve code cleanliness; delete unused icons.svg file.

--- React Project/src/pages/Home.jsx
+++ React Project/src/pages/Home.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
 import HeroCarousel from '../components/HeroCarousel';
 import ScrollSection from '../components/ScrollSection';
